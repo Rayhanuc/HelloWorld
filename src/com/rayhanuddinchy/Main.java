275b3fc4f32d20d62f19a5bee3d699e555e3a5d7
Rayhanuc <rayhanctg4@gmail.com>
2023-11-01 20:50:29 +0600

    Primitive Types

--- src/com/rayhanuddinchy/Main.java
+++ src/com/rayhanuddinchy/Main.java
@@ -2,7 +2,17 @@ package com.rayhanuddinchy;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 30;
-        System.out.println("Hello World");
+//        int age = 30;
+//        age = 35;
+
+//        int myAge = 30;
+//        int herAge = myAge;
+//        System.out.println(herAge);
+
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = flash;
     }
 }
