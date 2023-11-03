e71e77a896ae1bc36cf3b0cd7643bfae3abe48d6
Rayhanuc <rayhanctg4@gmail.com>
2023-11-04 03:55:51 +0600

    Reference Types, Primitive Types vs Reference Types

--- src/com/rayhanuddinchy/Main.java
+++ src/com/rayhanuddinchy/Main.java
@@ -1,7 +1,10 @@
 package com.rayhanuddinchy;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 //        int age = 30;
 //        age = 35;
 
@@ -9,10 +12,32 @@ public class Main {
 //        int herAge = myAge;
 //        System.out.println(herAge);
 
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = flash;
-    }
+//        byte age = 30;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.99F;
+//        char letter = 'A';
+//        boolean isEligible = flash;
+
+// ****** Reference Types
+//        byte age = 30;
+//        Date now = new Date();
+//        System.out.println("Now");
+//        System.out.println(now);
+
+// ****** Primitive Types vs Reference Types
+
+
+//    }
+     public static void main(String[] args) {
+// ****** Primitive Types vs Reference Types
+//         byte x = 1;
+//         byte y = x;
+//         x = 2;
+//         System.out.println(y);
+
+         Point point1 = new Point(1, 1);
+         Point point2 = point1;
+         point1.x = 2;
+         System.out.println(point2);
+     }
 }
