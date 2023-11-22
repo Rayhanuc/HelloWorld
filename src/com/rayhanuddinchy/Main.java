dab1b49e4220bce49ce2b5a8405d33ca86793157
Rayhanuc <rayhanctg4@gmail.com>
2023-11-23 03:17:52 +0600

    Comparison Operators

--- src/com/rayhanuddinchy/Main.java
+++ src/com/rayhanuddinchy/Main.java
@@ -196,29 +196,39 @@ public class Main {
 //        System.out.println("You are " + name);
 //    }
 
-    public static void main(String[] args) {
-// ****** Solution: Mortgage Calculator
-    final byte MONTHS_IN_YEAR = 12;
-    final byte PERCENT = 100;
+//    public static void main(String[] args) {
+//// ****** Solution: Mortgage Calculator
+//    final byte MONTHS_IN_YEAR = 12;
+//    final byte PERCENT = 100;
+//
+//    Scanner scanner = new Scanner(System.in);
+//
+//    System.out.println("Principal: ");
+//    int principal = scanner.nextInt();
+//
+//        System.out.println("Annual Interest Rate: ");
+//        float annualInterest = scanner.nextFloat();
+//        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+//
+//        System.out.println("Period (Years): ");
+//        byte years = scanner.nextByte();
+//        int numberOfPayments = years * MONTHS_IN_YEAR;
+//
+//        double mortgage = principal
+//                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
+//                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
+//
+//        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+//        System.out.print("Mortgage:" + mortgageFormatted);
+//    }
 
-    Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Principal: ");
-    int principal = scanner.nextInt();
 
-        System.out.println("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.println("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double mortgage = principal
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.print("Mortgage:" + mortgageFormatted);
+        public static void main(String[] args) {
+// ****** Comparison Operators
+    int x = 1;
+    int y = 1;
+//            System.out.println(x == y);
+            System.out.println(x != y);
     }
 }
