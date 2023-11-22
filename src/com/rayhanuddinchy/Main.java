9453466f7f4c28ec622a9d7509c7be6bf4b9e690
Rayhanuc <rayhanctg4@gmail.com>
2023-11-23 02:39:32 +0600

    Solution: Mortgage Calculator

--- src/com/rayhanuddinchy/Main.java
+++ src/com/rayhanuddinchy/Main.java
@@ -188,11 +188,37 @@ public class Main {
 //        System.out.println(result);
 //    }
 
+//    public static void main(String[] args) {
+//// ****** Reading Input
+//    Scanner scanner = new Scanner(System.in);
+//        System.out.print("Name: ");
+//    String name = scanner.nextLine().trim();
+//        System.out.println("You are " + name);
+//    }
+
     public static void main(String[] args) {
-// ****** Reading Input
+// ****** Solution: Mortgage Calculator
+    final byte MONTHS_IN_YEAR = 12;
+    final byte PERCENT = 100;
+
     Scanner scanner = new Scanner(System.in);
-        System.out.print("Name: ");
-    String name = scanner.nextLine().trim();
-        System.out.println("You are " + name);
+
+    System.out.println("Principal: ");
+    int principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.print("Mortgage:" + mortgageFormatted);
     }
 }
